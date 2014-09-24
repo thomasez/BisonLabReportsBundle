804ebccb3bcf45fdb0e0d6e03e6a4dc31bb40728
Thomas Lundquist <thomasez@redpill-linpro.com>
2014-09-24 20:30:52 +0200

    Method so that we can create and display URLs in the web based reports. Nice to be able to click directly to the data we list.

--- Lib/Reports/CommonReportFunctions.php
+++ Lib/Reports/CommonReportFunctions.php
@@ -5,10 +5,35 @@ namespace RedpillLinpro\SimpleReportsBundle\Lib\Reports;
 class CommonReportFunctions
 {
 
+    private $router;
+    private $manager;
+
     public function getManager()
     {
-        return $this->container->get('doctrine')->getManager();
+        if (!$this->manager) {
+            $this->manager = $this->container->get('doctrine')->getManager();
+        }
+        return $this->manager;
     } 
 
-}
+    public function getRouter()
+    {
+        if (!$this->router) {
+            $this->router = $this->container->get('router');
+        }
+        return $this->router;
+    }
 
+    public function createUrlFor($path, $values)
+    {
+        if (!is_array($values)) {
+            $values = array('id' => $values);
+        }
+
+        $router = $this->getRouter();
+        $route = $router->generate($path, $values);
+        $url = '<A HREF="' . $route . '">' . array_values($values)[0] . '</a>';
+    
+        return $url;
+    }
+}
